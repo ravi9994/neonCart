ecb0d697336b72f2c45d3bc42a568a1ed391fa5d
Ravi Gaudani <ravi.b.gaudani@gmail.com>
2023-06-08 11:08:47 +0530

    minor change

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit {
+  menuType: string;
+  constructor(
+    private menuController: MenuController
+  ) { }
 
-  constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {}
+  async toggleMenu(type) {
+    this.menuType = type;
+    this.menuController.toggle('end')
+  }
 
 }
