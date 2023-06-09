d98e688bfe3c07f92658410c1236730c4dae4c60
Ravi Gaudani <ravi.b.gaudani@gmail.com>
2023-06-09 10:48:38 +0530

    product detail image set

--- src/app/home/product-detail/product-detail.page.ts
+++ src/app/home/product-detail/product-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  goTo(url) {
+    this.router.navigate([url]);
+  }
 }
