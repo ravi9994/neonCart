d97cb61672a2c9a5de55a6b12c217f8f104f0206
Ravi Gaudani <ravi.b.gaudani@gmail.com>
2023-06-20 13:34:39 +0530

    set responsive

--- src/app/home/contact/contact.page.ts
+++ src/app/home/contact/contact.page.ts
@@ -17,7 +17,7 @@ export class ContactPage implements OnInit {
       let map = new google.maps.Map(document.getElementById('map_canvas'), {
         mapTypeControl: false,
         streetViewControl: false,
-        zoom: 30,
+        zoom: 10,
         center: { lat: 21.208348, lng: 72.846486 },
       });
     })
