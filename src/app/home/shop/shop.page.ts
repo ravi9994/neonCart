f9ab699e8da4ec0d879d6f584b5ca71e2238d26d
Ravi <ravi.b.gaudani@gmail.com>
2023-06-23 12:22:54 +0530

    shop and contact us page set

--- src/app/home/shop/shop.page.ts
+++ src/app/home/shop/shop.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { UtilService } from 'src/app/shared/services/util.service';
+import * as _ from 'lodash';
 import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay]);
 @Component({
@@ -17,7 +18,12 @@ export class ShopPage implements OnInit {
     this.getScreenSize();
   }
   swiper: any;
-  moveEnd = 50;
+  addMoreNumber = 9;
+  filterColors: any = [];
+  priceRangeValue = 100;
+  filterBrand: any = [];
+  touchTypeValue;
+  oldProductList;
   constructor(
     private platform: Platform,
     private router: Router,
@@ -47,114 +53,155 @@ export class ShopPage implements OnInit {
   ProductList = [
     {
       image: './../../assets/images/cameras.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      title: 'Canon 500 D',
+      detail: 'Super quality and features to this camera',
+      price: 685,
+      brands: 'Canon',
+      color: 'Black',
+      type: 'Camera'
     },
     {
-      image: './../../assets/images/shoose.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/camera2.png',
+      title: 'Canon 100D',
+      detail: 'Super quality and features to this camera',
+      price: 950,
+      brands: 'Canon',
+      color: 'Black',
+      type: 'Camera'
     },
     {
-      image: './../../assets/images/alexa.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/shirt1.jpeg',
+      title: "Men's shirts",
+      detail: 'Formal white shirt',
+      price: 100,
+      brands: 'PeterEngland',
+      color: 'White',
+      type: 'Shirt'
     },
     {
-      image: './../../assets/images/headphone.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/sirt2.jpeg',
+      title: "Men's shirts",
+      detail: 'Formal Grey shirt',
+      price: 200,
+      brands: 'RICHLOOK',
+      color: 'Grey',
+      type: 'Shirt'
     },
     {
-      image: './../../assets/images/music-watch.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/raincoat1.png',
+      title: 'BABA Raincoat',
+      detail: 'Double Kapad Raincort 100% waterproof',
+      price: 400,
+      brands: 'BABA',
+      color: 'Yellow',
+      type: 'Raincoat'
     },
     {
-      image: './../../assets/images/bluetooth.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/raincoat2.png',
+      title: 'BABA Raincoat',
+      detail: 'Double Kapad Raincort 100% waterproof',
+      price: 500,
+      brands: 'BABA',
+      color: 'Blue',
+      type: 'Raincoat'
     },
     {
-      image: './../../assets/images/powerback.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/pent1.jpeg',
+      title: "Men's pent",
+      detail: 'Formal mens pent for best quality and kapad',
+      price: 100,
+      brands: 'RICHLOOK',
+      color: 'Green',
+      type: 'Pent'
     },
     {
-      image: './../../assets/images/remote.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/pent2.jpeg',
+      title: "Men's pent",
+      detail: 'Formal mens pent for best quality and kapad',
+      price: 150,
+      brands: 'PeterEngland',
+      color: 'Black',
+      type: 'Pent'
     },
     {
-      image: './../../assets/images/cameras.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/sorts1.jpeg',
+      title: 'Sorts',
+      detail: 'Best sorts for mens and women both',
+      price: 800,
+      brands: 'Happywomen',
+      color: 'Black',
+      type: 'Sorts'
     },
     {
-      image: './../../assets/images/alexa.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/sorts2.jpeg',
+      title: 'Sorts',
+      detail: 'Best sorts for mens and women both',
+      price: 700,
+      brands: 'Happywomen',
+      color: 'Blue',
+      type: 'Sorts'
     },
     {
-      image: './../../assets/images/headphone.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
-    }, {
-      image: './../../assets/images/shoose.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+      image: './../../assets/images/light1.png',
+      title: 'Night Lamp',
+      detail: 'Night lamp for badroom and kitchen also.',
+      price: 50,
+      brands: 'Ujala',
+      color: 'White',
+      type: 'Light'
+    },
+    {
+      image: './../../assets/images/light2.png',
+      title: 'Night Lamp',
+      detail: 'Night lamp for badroom and kitchen also.',
+      price: 70,
+      brands: 'Ujala',
+      color: 'White',
+      type: 'Light'
     },
   ]
 
-  changeRange(ev) {
-    this.moveEnd = ev.target.value;
+  filterData() {
+    let filterData;
+
+    if (this.touchTypeValue) {
+      filterData = this.oldProductList.filter((obj) => obj.type == this.touchTypeValue);
+      this.touchTypeValue = false;
+    }
+
+    if (this.filterColors && this.filterColors.length > 0) {
+      if (filterData) {
+        filterData = filterData.filter((obj) => this.filterColors.includes(obj.color));
+      } else {
+        filterData = this.oldProductList.filter((obj) => this.filterColors.includes(obj.color));
+      }
+    }
+
+    if (this.filterBrand && this.filterBrand.length > 0) {
+      if (filterData) {
+        filterData = filterData.filter((obj) => this.filterBrand.includes(obj.brands));
+      } else {
+        filterData = this.oldProductList.filter((obj) => this.filterBrand.includes(obj.brands));
+      }
+    }
+
+    if (filterData) {
+      filterData = filterData.filter((obj) => obj.price <= this.priceRangeValue * 10);
+      this.ProductList = filterData;
+    } else {
+      filterData = this.oldProductList.filter((obj) => obj.price <= this.priceRangeValue * 10);
+      this.ProductList = filterData;
+    }
   }
 
   addMore() {
-    const value =
-    {
-      image: './../../assets/images/cameras.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+    if (this.addMoreNumber > 9) {
+      this.addMoreNumber = 1;
     }
-    const value1 =
-    {
-      image: './../../assets/images/alexa.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
+    for (let index = this.addMoreNumber; index < this.addMoreNumber + 4; index++) {
+      this.ProductList.push(this.oldProductList[index - 1]);
     }
-    const value2 =
-    {
-      image: './../../assets/images/headphone.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
-    }
-    const value3 =
-    {
-      image: './../../assets/images/shoose.png',
-      title: 'Speakers',
-      detail: 'Wireless Audio System Multiroom 360',
-      price: '$685.00'
-    }
-    this.ProductList.push(value);
-    this.ProductList.push(value1);
-    this.ProductList.push(value2);
-    this.ProductList.push(value3);
+    this.addMoreNumber = this.addMoreNumber + 4;
   }
 
   addToCart() {
@@ -170,6 +217,7 @@ export class ShopPage implements OnInit {
   }
 
   ngOnInit() {
+    this.oldProductList = _.cloneDeep(this.ProductList);
     this.platform.ready().then(() => {
       this.getScreenSize();
     });
@@ -179,12 +227,34 @@ export class ShopPage implements OnInit {
         disableOnInteraction: true,
       },
     });
-
-
-
   }
 
+  changeTouchValue(value) {
+    this.touchTypeValue = value;
+    this.filterData();
+  }
 
+  onChangeColors(event) {
+    let value = event.target.value;
+    if (event.target.checked) {
+      this.filterColors.push(value);
+    } else {
+      let findIndex = this.filterColors.findIndex(obj => obj === value);
+      this.filterColors.splice(findIndex, 1);
+    };
+    this.filterData();
+  }
+
+  onChangeBrands(event) {
+    let value = event.target.value;
+    if (event.target.checked) {
+      this.filterBrand.push(value);
+    } else {
+      let findIndex = this.filterBrand.findIndex(obj => obj === value);
+      this.filterBrand.splice(findIndex, 1);
+    };
+    this.filterData();
+  }
 
   getScreenSize() {
     const width = this.platform.width();
